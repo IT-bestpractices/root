a56955a4d495ef84589386a252d036b9dbe54519
Emily Ratliff <ratliff@localhost.localdomain>
2015-09-19 21:47:36 -0500

    Prototype IT Best Practices viewer implemented in Python with Flask

---
+++ tools/viewer/README.md
@@ -0,0 +1,54 @@
+# IT Best Practices Viewer
+
+This is a simple IT Best Practices viewer written in Python using Flask.
+It takes parameterized URLs and constructs a filesystem path from
+the request input strings. It outputs either the straight JSON or
+pretty printed HTML.
+
+The HTML generated is very rudimentary for now and just demonstrates what
+can be done.
+
+The request URLs are of the form
+http://127.0.0.1:5000/itbp/v1.0/show?app=os&domain=security&class=posix&os=linux&osname=redhat&tipname=nist_V-57569
+
+# Parameters
+The parameters match the file system hierarchy implemented in the IT Best
+Practices Rules.
+Require parameters include
+* app
+* domain
+* class
+* os
+* tipname
+
+Optional parameters include
+* release (currently unused)
+* osname 
+
+# Configuration
+The path to the IT Best Practices rules can be set in the itbp.cfg file which
+is signalled to the program by setting the ITBP_SETTINGS environment variable.
+
+# APIs Implemented
+show displays the pretty printed HTML output.
+showjson returns the file contents which are assumed to be JSON.
+
+# User stories implemented in this prototype
+* I am a system administrator and I want to view the Best Practice in a readable format. I was given the URL by a tool.
+* I am a tool and I want to retrieve the raw JSON of a particular Best Practice. I was given the URL by a tool or I generated it myself.
+
+# User story requested but not yet implemented
+* I am a system administrator and I want to view one of the best practices by the tag that was shared with my by another system administrator.
+
+# Input Sanitization
+The path is constructed out of arguments passed in by the user. Most of these
+elements are lowercased, secure_filename is used to sanitize the component 
+pieces. normpath is used to normalize the constructed relative path which
+is then joined to the path provided by the configuration. 
+
+Input is requested on whether this is sufficient sanitization. 
+
+If the file does not exist, an error is returned. The file is opened read only. 
+Typically, the viewer runs as a normal user and only files in the configured 
+path will be returned, so sensitive information will not be exposed if the 
+user uses the default configuration or configures another path properly.
---
+++ tools/viewer/itbp.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+# vim: smartindent tabstop=4 shiftwidth=4 expandtab number
+#
+# This file is part of the IT Best Practices project and was derived
+# from a file which is part of the Assimilation Project.
+#
+# Author: Emily Ratliff <eratliff@linuxfoundation.org>
+# Author: Alan Robertson <alanr@unix.sh>
+# Copyright (C) 2015 - Assimilation Systems Limited
+#
+# Free support is available from the Assimilation Project community - http://assimproj.org
+# Paid support is available from Assimilation Systems Limited - http://assimilationsystems.com
+#
+# The Assimilation software is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# The Assimilation software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the Assimilation Project software.  If not, see http://www.gnu.org/licenses/
+#
+#
+'''
+Prototype code for providing a REST interface for the IT Best Practices project.
+'''
+import sys
+import os, os.path
+sys.path.append('..')
+from flask import Flask, request, render_template, json, Response, abort
+from werkzeug.utils import secure_filename
+
+#configuration
+DEBUG = False
+TESTING = False
+ITBP_PATH = '/usr/share/itbp/v1.0/root/rules'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('ITBP_SETTINGS', silent=True)
+
+def validate_params(queryparms):
+    '''Parse the input query to find the path
+    Release and osname are optional components in the path
+    osname, tipname, and release don't have postfixes
+    tipname can have capitals
+    I trust ITBP_PATH since it is set by the person running the server'''
+    app_path = secure_filename(queryparms.get('app', '').lower()) + '.app'
+    domain_path = secure_filename(queryparms.get('domain', '').lower()) + '.domain'
+    class_path = secure_filename(queryparms.get('class', '').lower()) + '.class'
+    os_path = secure_filename(queryparms.get('os', '').lower()) + '.os'
+    osname_path = secure_filename(queryparms.get('osname', '').lower())
+    release_path = secure_filename(queryparms.get('release', '').lower())
+    tipname_path = secure_filename(queryparms.get('tipname', ''))
+    response_path = os.path.join(app_path, domain_path, class_path, os_path)
+    if not osname_path == "":
+        response_path = os.path.join(response_path, osname_path)
+    if not release_path == "":
+        response_path = os.path.join(response_path, release_path)
+    temp_path = os.path.join(response_path, tipname_path)
+    response_path = os.path.normpath(temp_path)
+    response_path = os.path.join(app.config['ITBP_PATH'], response_path)
+
+    return response_path
+
+@app.route('/')
+def hello_world():
+    'Dummy code for printing a static string on the root (/) page'
+    return 'Nothing to see here... Move along!'
+
+@app.route('/itbp/v1.0/querymeta', methods=['GET'])
+def query_meta():
+    '''Dummy code for testing the filename composition code'''
+    response_path = validate_params(request.args)
+    return 'Hello Query Metadata Path: "%s"!'  % response_path
+
+@app.route('/itbp/v1.0/showjson', methods=['GET'])
+def showjson():
+    '''Prototype code for requestion the JSON of a particular best practice.
+    The error cases are not handled correctly yet.
+    Open question - do we want to limit the size of the file that we show?
+    '''
+    itbpfilename = validate_params(request.args)
+    if not os.path.isfile(itbpfilename):
+        abort(404)
+    tipfile = open(itbpfilename, 'r')
+    dat = tipfile.read()
+    tipfile.close()
+    resp = Response(response=dat, status=200, mimetype='application/json')
+    return resp
+
+@app.route('/itbp/v1.0/show', methods=['GET'])
+def show():
+    '''Prototype code for showing a human readable HTML rendention
+    of and IT Best Practices criteria.
+    They all return apparent success, just very poor HTML.
+    '''
+    itbpfilename = validate_params(request.args)
+    if not os.path.isfile(itbpfilename):
+        abort(404)
+    (startpath, tipfilename) = os.path.split(itbpfilename)
+    tipfile = open(itbpfilename, 'r')
+    dat = tipfile.read()
+    tipfile.close()
+    new_obj = json.loads(dat)
+    sev = new_obj['severity']
+    tiptext = new_obj['text']
+    entiptext = tiptext['en']
+    shortdesc = entiptext['short_description']
+    longdesc = entiptext['long_description']
+    fixdesc = entiptext['fix']
+    checkdesc = entiptext['check']
+
+    return render_template('tip.html', severity=sev, tipname=tipfilename,
+                           shorttiptext=shortdesc, longtiptext=longdesc,
+                           checktiptext=checkdesc, fixtiptext=fixdesc)
+
+if __name__ == '__main__':
+
+    app.run()
